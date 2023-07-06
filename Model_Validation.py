7423294d1e462144d4b28b876da5b9f87f33b3fc
NaimiMariem <95921158+NaimiMariem@users.noreply.github.com>
2023-07-06 12:10:07 +0200

    Create Model_Validation.py

---
+++ Model_Validation.py
@@ -0,0 +1,47 @@
+#Metric used: Mean absolute error
+
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split    #function used to split data cf. last part
+
+
+###########################################
+#Classic main example
+import pandas as pd
+
+# Load data
+melbourne_file_path = '../input/melbourne-housing-snapshot/melb_data.csv'
+melbourne_data = pd.read_csv(melbourne_file_path) 
+# Filter rows with missing price values
+filtered_melbourne_data = melbourne_data.dropna(axis=0)
+# Choose target and features
+y = filtered_melbourne_data.Price
+melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'BuildingArea', 
+                        'YearBuilt', 'Lattitude', 'Longtitude']
+X = filtered_melbourne_data[melbourne_features]
+
+from sklearn.tree import DecisionTreeRegressor
+# Define model
+melbourne_model = DecisionTreeRegressor()
+# Fit model
+melbourne_model.fit(X, y)
+###########################################
+
+predicted_home_prices = melbourne_model.predict(X)
+mean_absolute_error(y, predicted_home_prices)
+
+#In order to avoid a problem when we compare the accuracy using the same data used for training
+#Solution; we can split up the data, to use a part for the training and another part for validation
+
+
+# split data into training and validation data, for both features and target
+# The split is based on a random number generator. Supplying a numeric value to
+# the random_state argument guarantees we get the same split every time we
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 0)
+# Define model
+melbourne_model = DecisionTreeRegressor()
+# Fit model
+melbourne_model.fit(train_X, train_y)
+
+# get predicted prices on validation data
+val_predictions = melbourne_model.predict(val_X)
+print(mean_absolute_error(val_y, val_predictions))
