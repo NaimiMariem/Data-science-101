e37c4057352f4731d4692bfe07aa025ca9175db8
NaimiMariem <95921158+NaimiMariem@users.noreply.github.com>
2023-07-06 15:32:27 +0200

    Random_forest.py

---
+++ Random_forest.py
@@ -0,0 +1,39 @@
+############################
+#Random forest model
+#The random forest helps us get better prediction than a signle regression tree
+#It helps us avoid under/overfitting
+############################
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+
+
+################################################
+#Classic main
+################################################
+# Load data
+melbourne_file_path = '../input/melbourne-housing-snapshot/melb_data.csv'
+melbourne_data = pd.read_csv(melbourne_file_path) 
+# Filter rows with missing values
+melbourne_data = melbourne_data.dropna(axis=0)
+# Choose target and features
+y = melbourne_data.Price
+melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'BuildingArea', 
+                        'YearBuilt', 'Lattitude', 'Longtitude']
+X = melbourne_data[melbourne_features]
+
+from sklearn.model_selection import train_test_split
+
+# split data into training and validation data, for both features and target
+# The split is based on a random number generator. Supplying a numeric value to
+# the random_state argument guarantees we get the same split every time we
+# run this script.
+train_X, val_X, train_y, val_y = train_test_split(X, y,random_state = 0)
+#################################################
+
+
+
+forest_model = RandomForestRegressor(random_state=1)
+forest_model.fit(train_X, train_y)
+melb_preds = forest_model.predict(val_X)
+print(mean_absolute_error(val_y, melb_preds))
